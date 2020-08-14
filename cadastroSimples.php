44d60163611a4e6bff035beebcf1d1a9445bd3f5
Cl3v3rs0n <cleverson.santos28@gmail.com>
2020-08-14 15:29:47 -0300

    Commit 2 de digite para cadastre

--- cadastroSimples.php
+++ cadastroSimples.php
@@ -3,12 +3,12 @@ $nome = 'nome';
 $usuario = 'usuario';
 $senha = 'senha';
 
-$nome = readline("Informe o usuário: \n");
+$nome = readline("cadastre o usuário: \n");
     if(($nome == $nome) && ($usuario ==$usuario) && ($senha == $senha)){
         echo "Olá " .$nome;
     }
 
-$senha = readline("\nDigite uma senha: \n");
+$senha = readline("\nCadastre uma senha: \n");
     if(($senha) != ($senha)){
         echo "Senha incorreta";
     }
