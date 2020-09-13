ffc4196afa1ca5b171420afc6b5b38e7856a9b25
Cleverson A Santos <cleverson.santos28@gmail.com>
2020-09-12 22:34:50 -0300

    first commit
    Arquivos PHP feitos em aula
    index.php
    resultadoParOUimpar.php

---
+++ Aula 04/resultadoParOUimpar.php
@@ -0,0 +1,17 @@
+<?php
+
+$primeiroNumero = $_GET['primeiroNumero'];
+$segundoNumero = $_GET['segundoNumero'];
+$soma = $_GET['soma'];
+
+$mensagem = "O resultado é $primeiroNumero $segundoNumero";
+
+echo "A soma dos números é: ", $mensagem;
+
+
+
+
+
+
+
+
