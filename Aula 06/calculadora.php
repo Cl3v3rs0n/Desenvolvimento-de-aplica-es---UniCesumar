e0b6e437a7ddc8440befd79d9736fa1c60275384
Cleverson A Santos <cleverson.santos28@gmail.com>
2020-09-12 22:39:01 -0300

    first commit
    Arquivos PHP feitos em sala junto com HTML
    calculadora.php
    formulario.html
    formulario2.html
    hello.html

---
+++ Aula 06/calculadora.php
@@ -0,0 +1,17 @@
+<php
+
+$valo1 = $_POST['valor1'];
+$valor2  = $_POST['valor2];
+$operacao = $_POST['operacao'];
+
+$soma = $valor1 + $valor2;
+    echo "O resultado da soma é: $operacao \n";
+
+$subtracao = $valor1 - $valor2;
+echo "O resultado da soma é: $operacao \n";
+
+$multiplicacao = $valor1 * $valor2;
+    echo "O resultado da soma é: $operacao \n";
+
+$divisao = $valor1 / $valor2;
+    echo "O resultado da soma é: $operacao \n";
